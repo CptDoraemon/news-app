34215c562be8da9311f38797d81f286b6062c53a
CptDoraemon <yxx0827@gmail.com>
2020-01-02 17:48:24 -0500

    Updates

---
+++ precache-manifest.3881543e70f39ac9942e9218de42b67f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a7f7ae03609912421650d5204f4a0d4",
+    "url": "/news-app/index.html"
+  },
+  {
+    "revision": "29867ebf1d3d1b4f9011",
+    "url": "/news-app/static/css/main.4d1b16e9.chunk.css"
+  },
+  {
+    "revision": "8d4a83ab95eee3a85986",
+    "url": "/news-app/static/js/2.4123fa7e.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/news-app/static/js/2.4123fa7e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "29867ebf1d3d1b4f9011",
+    "url": "/news-app/static/js/main.4d126256.chunk.js"
+  },
+  {
+    "revision": "bbf9496c52d0bafb0e00",
+    "url": "/news-app/static/js/runtime-main.b5f207f7.js"
+  }
+]);
