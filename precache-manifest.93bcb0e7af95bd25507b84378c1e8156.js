6902da42a14f9c4155e605eb76e42c06fd3b88a9
Xiaoxi <yxx0827@gmail.com>
2020-03-13 20:39:31 -0400

    Updates

--- precache-manifest.93bcb0e7af95bd25507b84378c1e8156.js
+++ precache-manifest.93bcb0e7af95bd25507b84378c1e8156.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a27ec13cb0e9cf8832927c7ba90b0dd9",
+    "revision": "3984aca22598c0c53f08a253bbbd2b26",
     "url": "/news-app/index.html"
   },
   {
-    "revision": "3827fd3cce42f3551d6d",
+    "revision": "236566de5e8722afab7d",
     "url": "/news-app/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "6f816a9da2934ebf04d4",
-    "url": "/news-app/static/js/2.40bdba5e.chunk.js"
+    "revision": "75f3354aaaad9f0265e1",
+    "url": "/news-app/static/js/2.b3730c24.chunk.js"
   },
   {
     "revision": "b9bc2fc7725df2ce4b7409c91212bc51",
-    "url": "/news-app/static/js/2.40bdba5e.chunk.js.LICENSE"
+    "url": "/news-app/static/js/2.b3730c24.chunk.js.LICENSE"
   },
   {
-    "revision": "3827fd3cce42f3551d6d",
-    "url": "/news-app/static/js/main.3355938f.chunk.js"
+    "revision": "236566de5e8722afab7d",
+    "url": "/news-app/static/js/main.f5324e44.chunk.js"
   },
   {
     "revision": "bbf9496c52d0bafb0e00",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/news-app/precache-manifest.10b18741418264ac1451cf7d102b8d26.js"
+  "/news-app/precache-manifest.93bcb0e7af95bd25507b84378c1e8156.js"
 );
 
 self.addEventListener('message', (event) => {
