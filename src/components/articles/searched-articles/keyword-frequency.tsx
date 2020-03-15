5eef480d1aa2e1ddd9d648d7966dbeb4dcbd8a9f
Xiaoxi <yxx0827@gmail.com>
2020-03-14 20:31:19 -0400

    update chart

--- src/components/articles/searched-articles/keyword-frequency.tsx
+++ src/components/articles/searched-articles/keyword-frequency.tsx
@@ -22,6 +22,8 @@ const KeywordFrequency: React.FC<KeywordFrequencyProps> = ({bin, frequency}) => 
     const theme = useTheme();
     const lightColor = theme.palette.secondary.light;
     const darkColor = theme.palette.secondary.dark;
+    const primaryColor = theme.palette.primary.light;
+    const backgroundColor = '#EEE';
     const TRANSITION_DELAY = 500;
     const BAR_TRANSITION_DURATION = 2000;
 
@@ -77,28 +79,24 @@ const KeywordFrequency: React.FC<KeywordFrequencyProps> = ({bin, frequency}) => 
         const barY = (d: any) => chartY + chartHeight - barHeight(d);
 
         const svg = d3.select(`#${id}`).append('svg');
-        const bars = svg
+        const bars = svg.append('g')
+            .selectAll('rect').data(frequency)
+            .enter().append('rect');
+        const hoverDetectionBars = svg
+            .append('g')
             .selectAll('rect').data(frequency)
             .enter().append('rect');
 
         svg.attr('width', svgWidth)
             .attr('height', svgHeight)
-            .style('background', '#EEE');
+            .style('background', backgroundColor);
 
         bars.style('fill', lightColor)
             .style('opacity', 0.8)
             .attr('width', barWidth)
             .attr('height', 0)
             .attr('x', barX)
-            .attr('y', chartY + chartHeight)
-            .on('mouseover', function(data) {
-                d3.select(this)
-                    .style('opacity', 1)
-            })
-            .on('mouseout', function(data) {
-                d3.select(this)
-                    .style('opacity', 0.8)
-            });
+            .attr('y', chartY + chartHeight);
 
         bars.transition()
             .attr('height', barHeight)
@@ -107,7 +105,16 @@ const KeywordFrequency: React.FC<KeywordFrequencyProps> = ({bin, frequency}) => 
             .duration(BAR_TRANSITION_DURATION)
             .ease(d3.easeElastic);
 
+        hoverDetectionBars
+            .style('fill', lightColor)
+            .style('opacity', 0)
+            .attr('width', barWidth)
+            .attr('height', chartHeight)
+            .attr('x', barX)
+            .attr('y', chartY);
 
+
+        // Axes
         const yAxisScale = d3.scaleLinear()
             .domain([0, maxFrequency])
             .range([chartHeight, 0]);
@@ -149,6 +156,73 @@ const KeywordFrequency: React.FC<KeywordFrequencyProps> = ({bin, frequency}) => 
 
         yAxis(yLegends);
         xAxis(xLegends);
+
+        // Grid
+        let grid = svg.append('g');
+        const gridLine = d3.axisLeft(yAxisScale);
+        grid.style('opacity', 0)
+            .style('transform', `translate(${chartX}px, ${chartY}px)`);
+        gridLine
+            .tickFormat(d3.format("d"))
+            .tickSizeInner(-chartWidth-6)
+            .tickSizeOuter(0)
+            .tickPadding(9);
+
+        // Hover Date Text
+        let hoverDateText = svg.append('g');
+        const hoverDateTextAxis = d3.axisBottom(xAxisScale);
+        hoverDateText.style('opacity', 0)
+            .style('transform', `translate(${chartX}px, ${chartHeight + chartY}px)`);
+        hoverDateTextAxis
+            // @ts-ignore
+            .tickFormat((d) => binDateStringArray[d]);
+
+        // Hover Frequency Text
+        let hoverFreqText = svg.append('g');
+        const hoverFreqTextAxis = d3.axisBottom(xAxisScale);
+        hoverFreqText.style('opacity', 0);
+        hoverFreqTextAxis
+            // @ts-ignore
+            .tickFormat((d) => frequency[d])
+            .tickSize(0)
+            .tickPadding(-9);
+
+        hoverDetectionBars
+            .on('mouseover', function(d, i) {
+                gridLine
+                    .tickValues([d]);
+                gridLine(grid);
+                grid.selectAll(".tick line")
+                    .style('transform', `translate(-6px, 0)`)
+                    .style('color', primaryColor);
+                grid.selectAll(".domain").remove();
+                grid.style('opacity', 1);
+
+                hoverDateTextAxis
+                    .tickValues([i]);
+                hoverDateTextAxis(hoverDateText);
+                hoverDateText.selectAll(".domain").remove();
+                hoverDateText.style('opacity', 1);
+
+                hoverFreqTextAxis
+                    .tickValues([i]);
+                hoverFreqTextAxis(hoverFreqText);
+                hoverFreqText.selectAll(".domain").remove();
+                hoverFreqText
+                    .style('transform', `translate(${chartX}px, ${yAxisScale(d) + chartY}px)`)
+                    .style('opacity', 1);
+
+                const bar = bars.filter((d, index) => index === i);
+                bar.style('fill', primaryColor);
+        })
+            .on('mouseout', function(data, i) {
+                grid.style('opacity', 0);
+                hoverDateText.style('opacity', 0);
+
+                bars.filter((d, index) => index === i)
+                    .style('fill', lightColor);
+
+            });
 
 
     }, [wrapperRef]);
