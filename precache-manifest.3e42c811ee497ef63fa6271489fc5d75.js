c947cf6a6fae203b4689722da1b018555aff6fbc
Xiaoxi <yxx0827@gmail.com>
2020-03-18 03:34:43 -0400

    Updates

--- precache-manifest.3e42c811ee497ef63fa6271489fc5d75.js
+++ precache-manifest.3e42c811ee497ef63fa6271489fc5d75.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f2305b26eb27575f7d1c4c6549344b98",
+    "revision": "afde64b380819ed4df4ef389c170c830",
     "url": "/news-app/index.html"
   },
   {
-    "revision": "f286c2876b55e40565fa",
+    "revision": "85e39fd6f910be0542e8",
     "url": "/news-app/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "99037601c1dafaf1fabd",
-    "url": "/news-app/static/js/2.3b7e5169.chunk.js"
+    "revision": "c6c57d1f3793a04eec37",
+    "url": "/news-app/static/js/2.50a67622.chunk.js"
   },
   {
     "revision": "b9bc2fc7725df2ce4b7409c91212bc51",
-    "url": "/news-app/static/js/2.3b7e5169.chunk.js.LICENSE"
+    "url": "/news-app/static/js/2.50a67622.chunk.js.LICENSE"
   },
   {
-    "revision": "f286c2876b55e40565fa",
-    "url": "/news-app/static/js/main.313674bf.chunk.js"
+    "revision": "85e39fd6f910be0542e8",
+    "url": "/news-app/static/js/main.9caad6a6.chunk.js"
   },
   {
     "revision": "bbf9496c52d0bafb0e00",
