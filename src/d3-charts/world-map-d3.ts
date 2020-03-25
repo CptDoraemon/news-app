c42bc7880c6a06639873335d1258904db07b955f
Xiaoxi <yxx0827@gmail.com>
2020-03-25 18:45:16 -0400

    update world map

--- src/d3-charts/world-map-d3.ts
+++ src/d3-charts/world-map-d3.ts
@@ -30,9 +30,26 @@ class WorldMapD3 {
     color = {
         red: '#ef5350',
         getMapColor: (percentage: number) => `rgba(255, 166, 0, ${percentage.toFixed(1)})`,
-        grey: "#eee"
+        grey: "#eee",
+        mapStrokeNormal: '#aaa',
+        mapStrokeHoverHighlight: '#222',
+        mapStrokeHoverDim: 'rgba(0,0,0,0)',
     };
     monthStrings = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    getStrokeWidth = () => this.dimension.svgWidth >= 1000 ? 1 : 0.5;
+    animations = {
+      zoomIn: {
+          delay: 0,
+          duration: 3000,
+      },
+      changeMapColor: {
+          delay: 3000,
+          duration: 1000,
+      },
+      timeLapse: {
+          delay: 4000,
+      }
+    };
     id: string;
     dimension: {
         svgWidth: number,
@@ -68,7 +85,21 @@ class WorldMapD3 {
     };
     state: {
         time: number,
-        timeMax: number
+        timeMax: number,
+        tooltipData: {
+            inputX: number, // top left x of map path rect, used as primary anchor
+            inputXSecondary: number, // top right x of map path rect, used in case primary anchor causing tooltip overlaps map rect
+            inputY: number, // top right y of map path rect
+            inputYSecondary: number, // bottom right y of map path rect
+            data: {
+                country: string,
+                case: {
+                    cases: number[],
+                    deaths: number[],
+                    recovered: number[],
+                } | undefined
+            }
+        }
     };
 
     constructor(id: string, width: number, themeColor: string, caseData: CovidCaseData) {
@@ -88,7 +119,17 @@ class WorldMapD3 {
         };
         this.state = {
             time: 0,
-            timeMax: 0
+            timeMax: 0,
+            tooltipData: {
+                inputX: -1000,
+                inputXSecondary: -1000,
+                inputY: -1000,
+                inputYSecondary: -1000,
+                data: {
+                    country: '',
+                    case: undefined
+                }
+            }
         }
     }
 
@@ -177,6 +218,7 @@ class WorldMapD3 {
             .style('font-size', '0.875rem')
             .style('fill', '#fff')
             .attr("text-anchor", "middle")
+            // .attr('alignment-baseline', 'bottom')
         // tspanCountry
         // tspanDate
         // tspanCaseAccumulative
@@ -206,76 +248,104 @@ class WorldMapD3 {
             tspanRecoveredNew
         };
 
-        this.updateTooltip(200, 200, {
-            country: 'country',
-            date: 'date',
-            caseAccumulative: 1,
-            deathAccumulative: 2,
-            recoveredAccumulative: 3,
-            caseNew: 11,
-            deathNew: 22,
-            recoveredNew: 33
-        })
+        this.updateTooltip()
 
     }
 
-    updateTooltip(
-        inputX: number,
-        inputY: number,
-        data: {
-            country: string,
-            date: string,
-            caseAccumulative: number,
-            deathAccumulative: number,
-            recoveredAccumulative: number,
-            caseNew: number,
-            deathNew: number,
-            recoveredNew: number
-        }
-    ) {
+    updateTooltip() {
         if (!this.references.tooltip) return;
 
+        const {
+            inputX, // top left x of map path rect, used as primary anchor
+            inputXSecondary, // top right x of map path rect, used in case primary anchor causing tooltip overlaps map rect
+            inputY, // top right y of map path rect
+            inputYSecondary, // bottom right y of map path rect
+            data
+        } = this.state.tooltipData;
+
+        const getNew = (num: number) => {
+            return num >= 0 ? ` (+${num})` : ` (${num})`
+        };
+
+        const currentT = this.state.time;
+        const date = new Date(this.data.case.series[currentT]);
+        const dateText = `${date.getDate()} ${this.monthStrings[date.getMonth()]}, ${date.getFullYear()}`;
+        const noData = 'Unknown';
+
         this.references.tooltip.tspanCountry.text(data.country);
-        this.references.tooltip.tspanDate.text(data.date);
-        this.references.tooltip.tspanCaseAccumulative.text(`Confirmed: ${data.caseAccumulative}`);
-        this.references.tooltip.tspanCaseNew.text(data.caseNew >= 0 ? ` (+${data.caseNew})` : ` (${data.caseNew})`);
-        this.references.tooltip.tspanDeathAccumulative.text(`Death: ${data.deathAccumulative}`);
-        this.references.tooltip.tspanDeathNew.text(data.deathNew >= 0 ? ` (+${data.deathNew})` : ` (${data.deathNew})`);
-        this.references.tooltip.tspanRecoveredAccumulative.text(`Recovered: ${data.recoveredAccumulative}`);
-        this.references.tooltip.tspanRecoveredNew.text(data.recoveredNew >= 0 ? ` (+${data.recoveredNew})` : ` (${data.recoveredNew})`);
+        this.references.tooltip.tspanDate.text(dateText);
+        if (!data.case) {
+            // no data
+            this.references.tooltip.tspanCaseAccumulative.text(`Confirmed: ${noData}`);
+            this.references.tooltip.tspanCaseNew.text('');
+            this.references.tooltip.tspanDeathAccumulative.text(`Death: ${noData}`);
+            this.references.tooltip.tspanDeathNew.text('');
+            this.references.tooltip.tspanRecoveredAccumulative.text(`Recovered: ${noData}`);
+            this.references.tooltip.tspanRecoveredNew.text('');
+        } else {
+            this.references.tooltip.tspanCaseAccumulative.text(`Confirmed: ${data.case.cases[currentT]}`);
+            this.references.tooltip.tspanCaseNew.text(currentT === 0 ? getNew(0) : getNew(data.case.cases[currentT] - data.case.cases[currentT - 1]));
+            this.references.tooltip.tspanDeathAccumulative.text(`Death: ${data.case.deaths[currentT]}`);
+            this.references.tooltip.tspanDeathNew.text(currentT === 0 ? getNew(0) : getNew(data.case.deaths[currentT] - data.case.deaths[currentT - 1]));
+            this.references.tooltip.tspanRecoveredAccumulative.text(`Recovered: ${data.case.recovered[currentT]}`);
+            this.references.tooltip.tspanRecoveredNew.text(currentT === 0 ? getNew(0) : getNew(data.case.recovered[currentT] - data.case.recovered[currentT - 1]));
+        }
 
         const bBox = this.references.tooltip.tooltip.node()?.getBBox();
         if (!bBox) return;
         const p = 0.2;
-        const x = inputX - bBox.width*0.5*(1+0.5*p);
-        const y = inputY;
+        const shift = 5;
+        const tooltipWidth = (1+p)*bBox.width;
+        const tooltipHeight = (1+p)*bBox.height;
 
+        const xBg = inputX - tooltipWidth - shift;
+        const xBgFloored = Math.max(this.dimension.m.l, xBg);
+        let textX, tooltipX;
+        let y = inputY;
+        if (xBgFloored + tooltipWidth > inputX) {
+            // overlapping with map rect
+            tooltipX = inputXSecondary + shift;
+            if (tooltipX + tooltipWidth > this.dimension.svgWidth) {
+                // tooltip overflows viewport right edge now
+                // fallback to use primary x and use secondary y
+                tooltipX = xBgFloored;
+                textX = tooltipX + 0.5*tooltipWidth;
+                y = inputYSecondary
+            } else {
+                textX = tooltipX + 0.5*tooltipWidth
+            }
+        } else {
+            tooltipX = xBgFloored;
+            textX = tooltipX + 0.5*tooltipWidth
+        }
+
+        y = Math.min(y, this.dimension.svgHeight - tooltipHeight);
         this.references.tooltip.tooltip
-            .attr('x', x)
+            .attr('x', textX)
             .attr('y', y)
         this.references.tooltip.tspanCountry
-            .attr('x', x)
-            .attr('dy', 0)
+            .attr('x', textX)
+            .attr('dy', '0.875em')
         this.references.tooltip.tspanDate
-            .attr('x', x)
-            .attr('dy', '1rem')
+            .attr('x', textX)
+            .attr('dy', '1em')
         this.references.tooltip.tspanCaseAccumulative
-            .attr('x', x)
-            .attr('dy', '1rem')
+            .attr('x', textX)
+            .attr('dy', '1em')
 
         this.references.tooltip.tspanDeathAccumulative
-            .attr('x', x)
-            .attr('dy', '1rem')
+            .attr('x', textX)
+            .attr('dy', '1em')
 
         this.references.tooltip.tspanRecoveredAccumulative
-            .attr('x', x)
-            .attr('dy', '1rem')
+            .attr('x', textX)
+            .attr('dy', '1em')
 
         this.references.tooltip.bg
-            .attr('x', x - bBox.width*(1+p)*0.5)
-            .attr('y', inputY - p*bBox.height)
-            .attr('width', bBox.width * (1+p))
-            .attr('height', bBox.height * (1+p))
+            .attr('x', tooltipX)
+            .attr('y', y - p/2*bBox.height)
+            .attr('width', tooltipWidth)
+            .attr('height', tooltipHeight)
     }
 
     initMap() {
@@ -292,22 +362,34 @@ class WorldMapD3 {
             .attr("d", d3.geoPath()
                 .projection(projection)
             )
-            .style("stroke", "#222")
-            .style("stroke-width", this.dimension.svgWidth >= 1000 ? 1 : 0.5)
+            .style("stroke", this.color.mapStrokeNormal)
+            .style("stroke-width", this.getStrokeWidth())
             .style('opacity', 0)
             .style('transform', 'scale(5)')
             .style('transform-origin', '50% 50%');
+    }
 
+    mapZoomIn() {
+        if (!this.references.mapPaths) return;
         this.references.mapPaths.transition()
             .delay((d, i) => (i % 10) * 200)
             .duration(1000)
             .style('opacity', 1)
             .style('transform', 'scale(1)');
 
+        // change map color from grey to white
+        this.references.mapPaths.transition()
+            .delay(this.animations.changeMapColor.delay)
+            .duration(this.animations.changeMapColor.duration)
+            .style('fill', this.color.getMapColor(0))
+    }
+
+    initDateText() {
+        if (!this.references.svg) return;
         this.references.dateText = this.references.svg.append("g").append("text")
             .attr('x', this.dimension.svgWidth * 0.5)
             .attr('y', this.dimension.svgWidth >= 1000 ? this.dimension.svgHeight - 30 : this.dimension.svgHeight - 15)
-            .attr('font-size', this.dimension.svgWidth >= 1000 ? '2rem' : '0.875rem')
+            .attr('font-size', this.dimension.svgWidth >= 1000 ? '1.5rem' : '0.875rem')
             .attr('font-weight', 700)
             .attr('text-anchor', 'middle')
         ;
@@ -345,6 +427,7 @@ class WorldMapD3 {
             if (this.state.time <= this.state.timeMax) {
                 updateMap();
                 updateDateText();
+                this.updateTooltip();
                 if (this.state.time < this.state.timeMax) {
                     this.state.time++;
                     setTimeout(updateState, gap)
@@ -362,30 +445,84 @@ class WorldMapD3 {
             const bBox = this.getBBox();
             const x = bBox.x;
             const y = bBox.y;
-            const time = thisClass.state.time;
-            const date = new Date(thisClass.data.case.series[thisClass.state.time]);
-            thisClass.updateTooltip(x, y, {
-                country: d.properties.name,
-                date: `${date.getDate()} ${thisClass.monthStrings[date.getMonth()]}, ${date.getFullYear()}`,
-                caseAccumulative: d.properties.case ? d.properties.case.cases[time] : 0,
-                deathAccumulative: d.properties.case ? d.properties.case.deaths[time] : 0,
-                recoveredAccumulative: d.properties.case ? d.properties.case.recovered[time] : 0,
-                caseNew: !d.properties.case ? 0 : time === 0 ? 0 : d.properties.case.cases[time] - d.properties.case.cases[time-1],
-                deathNew: !d.properties.case ? 0 : time === 0 ? 0 : d.properties.case.deaths[time] - d.properties.case.deaths[time-1],
-                recoveredNew: !d.properties.case ? 0 : time === 0 ? 0 : d.properties.case.recovered[time] - d.properties.case.recovered[time-1],
-            })
+            const xSecondary = bBox.x + bBox.width;
+            const ySecondary = bBox.y + bBox.height;
+
+            // tooltip
+            if (!d.properties.case) {
+                // no data for this country
+                thisClass.state.tooltipData = Object.assign(
+                    thisClass.state.tooltipData,
+                    {
+                        inputX: x,
+                        inputXSecondary: xSecondary,
+                        inputY: y,
+                        inputYSecondary: ySecondary,
+                        data: {
+                            country: d.properties.name,
+                            case: null
+                        }
+                })
+            } else {
+                thisClass.state.tooltipData = Object.assign(
+                    thisClass.state.tooltipData,
+                    {
+                        inputX: x,
+                        inputXSecondary: xSecondary,
+                        inputY: y,
+                        inputYSecondary: ySecondary,
+                        data: {
+                            country: d.properties.name,
+                            case: {...d.properties.case}
+                        }
+                    }
+                );
+                //     thisClass.updateTooltip(x, xSecondary, y, ySecondary, {
+                //     country: d.properties.name,
+                //     date: `${date.getDate()} ${thisClass.monthStrings[date.getMonth()]}, ${date.getFullYear()}`,
+                //     caseAccumulative: d.properties.case.cases[time],
+                //     deathAccumulative: d.properties.case.deaths[time],
+                //     recoveredAccumulative: d.properties.case.recovered[time],
+                //     caseNew: time === 0 ? 0 : d.properties.case.cases[time] - d.properties.case.cases[time-1],
+                //     deathNew: time === 0 ? 0 : d.properties.case.deaths[time] - d.properties.case.deaths[time-1],
+                //     recoveredNew: time === 0 ? 0 : d.properties.case.recovered[time] - d.properties.case.recovered[time-1],
+                // })
+            }
+            thisClass.updateTooltip();
+
+            // country path
+            const thisCountryName = d.properties.name;
+            // thisClass.references.mapPaths
+            //     ?.style('stroke-width', (_) => {
+            //         return _.properties.name === thisCountryName ? 5 : thisClass.dimension.svgWidth >= 1000 ? 1 : 0.5
+            //     })
+            // d3.select(this)
+            //     .style('stroke', thisClass.color.mapStrokeHoverHighlight)
+            d3.select(this)
+                .style('stroke-width', 5)
+
         })
             .on('mouseleave', function(d) {
-                thisClass.updateTooltip(-1000, -1000, {
-                    country: '',
-                    date: ``,
-                    caseAccumulative: 0,
-                    deathAccumulative: 0,
-                    recoveredAccumulative: 0,
-                    caseNew: 0,
-                    deathNew: 0,
-                    recoveredNew: 0,
-                })
+                thisClass.state.tooltipData = Object.assign(
+                    thisClass.state.tooltipData,
+                    {
+                        inputX: -1000,
+                        inputXSecondary: -1000,
+                        inputY: -1000,
+                        inputYSecondary: -1000,
+                        data: {
+                            country: '',
+                            case: null
+                        }
+                    }
+                );
+                thisClass.updateTooltip();
+
+
+                // country path
+                // d3.select(this).style('stroke', thisClass.color.mapStrokeNormal)
+                // const thisCountryName = d.properties.name;
+                thisClass.references.mapPaths?.style("stroke-width", thisClass.getStrokeWidth())
             })
     }
 
@@ -394,9 +531,14 @@ class WorldMapD3 {
             await this.getData();
             this.initSvg();
             this.initMap();
+            this.initDateText();
             this.initTooltip();
+            this.mapZoomIn();
             this.appendMouseEvent();
-            setTimeout(() => this.startTimeLapse(), 4000);
+            setTimeout(
+                () => {
+                    this.startTimeLapse()
+                }, this.animations.timeLapse.delay);
         } catch (e) {
             console.log(e);
         }
