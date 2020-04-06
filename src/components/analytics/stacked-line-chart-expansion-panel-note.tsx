7c4590e08a4c0ef622bc1ebcf9ffba723c38d923
Xiaoxi <yxx0827@gmail.com>
2020-04-06 03:26:01 -0400

    improve expandable panel

--- src/components/analytics/stacked-line-chart-expansion-panel-note.tsx
+++ src/components/analytics/stacked-line-chart-expansion-panel-note.tsx
@@ -1,14 +1,13 @@
 import React, {useEffect, useState} from "react";
 import makeStyles from "@material-ui/core/styles/makeStyles";
-import {ExpansionPanel, ExpansionPanelDetails, ExpansionPanelSummary} from "@material-ui/core";
+import {ExpansionPanel, ExpansionPanelDetails, ExpansionPanelSummary, withStyles} from "@material-ui/core";
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
-import RemoveCircleOutlineIcon from '@material-ui/icons/RemoveCircleOutline';
 import Title from "./utilitis/title";
 import Content from "./utilitis/content";
 
 const contentParagraphs = [
     'The band height of each category is the percentage of news count in that category to the total news count in that day. ' +
-    'Given the number of news archived each day fluctuate and follow a weekly pattern, the percentage tells a better story that how people\'s interest in different news change over time.',
+    'Given the number of news archived each day often fluctuates and follows a weekly pattern, the percentage tells a better story that how people\'s interests in different news change over time.',
     'Some patterns can be observed from the graph: due to the spreading of the novel coronavirus at the beginning of 2020, more and more health news were being reported.',
     'Since the early March of 2020, many countries and cities in Europe and North America starts to lockdown, at the same time we see a decline in sport and entertainment news.'
 ];
@@ -20,17 +19,12 @@ const useStyles = makeStyles(theme => ({
         backgroundColor: 'transparent',
         '&:before': {
             height: 0
+        },
+        '&$rootExpanded': {
+            margin: 0
         }
     },
-    rootExpanded: {
-        margin: 0
-    },
-    summaryRoot: {
-        width: '100%',
-    },
-    summaryExpanded: {
-        minHeight: 48
-    },
+    rootExpanded: {},
     summaryWrapper: {
         width: '100%',
         display: 'flex',
@@ -55,6 +49,29 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
+const summaryStyle = () => {
+    return {
+        root: {
+            minHeight: 48,
+            '&$expanded': {
+                minHeight: 48,
+            },
+        },
+        expanded: {},
+        content: {
+            margin: '12px 0',
+            '&$expanded': {
+                margin: '12px 0',
+            },
+        },
+    };
+};
+
+const StyledExpansionPanelSummary = withStyles(summaryStyle)(
+    ExpansionPanelSummary
+);
+
+
 interface ExpansionPanelNoteProps {
 
 }
@@ -67,6 +84,14 @@ const StackedLineChartExpansionPanelNote: React.FC<ExpansionPanelNoteProps> = ()
         setExpanded((prevState) => !prevState)
     };
 
+    useEffect(() => {
+        // the component after this has scroll linked animation
+        setTimeout(() => {
+            window.scrollTo(window.scrollX, window.scrollY + 1);
+            window.scrollTo(window.scrollX, window.scrollY - 1)
+        }, 300)
+    }, [expanded]);
+
     return (
         <ExpansionPanel
             expanded={expanded}
@@ -75,18 +100,19 @@ const StackedLineChartExpansionPanelNote: React.FC<ExpansionPanelNoteProps> = ()
                 root: classes.root,
                 expanded: classes.rootExpanded
             }}>
-            <ExpansionPanelSummary
-                classes={{
-                    root: classes.summaryRoot,
-                    expanded: classes.summaryExpanded
-                }}
-            >
+            <StyledExpansionPanelSummary>
                 <div className={classes.summaryWrapper}>
                     <div className={classes.line}> </div>
-                    { expanded ? <RemoveCircleOutlineIcon style={{fontSize: '2rem'}}/> : <AddCircleOutlineIcon style={{fontSize: '2rem'}}/> }
+                    <AddCircleOutlineIcon
+                        style={{
+                            fontSize: '2rem',
+                            transition: 'transform 0.3s',
+                            transform: expanded ? 'rotate(135deg)' : 'rotate(0deg)'
+                        }}
+                    />
                     <div className={classes.line}> </div>
                 </div>
-            </ExpansionPanelSummary>
+            </StyledExpansionPanelSummary>
             <ExpansionPanelDetails className={classes.detailWrapper}>
                 <Title value={['How to use this graph?']}/>
                 {
